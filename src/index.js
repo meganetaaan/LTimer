b141d3adf99d896399ad43fa5fc23a764d4aafb8
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2016-03-01 23:25:12 +0900

    fixed h5 library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const jQuery = require('jquery');
-const h5 = require('../lib/hifive/h5-1.2.1.dev.js');
+const h5 = require('../lib/hifive/h5.dev.js');
 const timer = require('./timer');
 require('./timerController');
