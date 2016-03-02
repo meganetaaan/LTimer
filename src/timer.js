9c851fc3d1e8143ce69ef65ba04b6d8b52a96c9e
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2016-03-03 01:25:01 +0900

    fixed state transition bug, added resetBtn

--- src/timer.js
+++ src/timer.js
@@ -1,95 +1,97 @@
-'use strict';
-const DEFAULT_INTERVAL = 300000;
-let interval = DEFAULT_INTERVAL;
-let startTime = 0;
-let status = interval;
-let lastStatus = interval;
-let timeout = null;
-let render = (timer)=>{};
-let renderer = {render: ()=>{}};
-let onSecondCallback = second => {};
-let onStartCallback = second => {};
-let onStopCallback = () => {};
-const nextFrame = window ? window.requestAnimationFrame : setImmediate;
-const cancelFrame = window ? window.cancelAnimationFrame : cancelImmediate;
+(function(){
+  'use strict';
+  const DEFAULT_INTERVAL = 300000;
+  let limit = DEFAULT_INTERVAL;
+  let interval = DEFAULT_INTERVAL;
+  let startTime = 0;
+  let lastStoppedTime = interval;
+  let status = interval;
+  let lastStatus = interval;
+  let timeout = null;
+  let render = (timer)=>{};
+  let renderer = {render: ()=>{}};
+  let onSecondCallback = second => {};
+  let onStartCallback = second => {};
+  let onStopCallback = () => {};
+  const nextFrame = window ? window.requestAnimationFrame : setImmediate;
+  const cancelFrame = window ? window.cancelAnimationFrame : cancelImmediate;
 
-function setRenderer(_renderer){
-  renderer = _renderer;
-}
-
-function setOnSecondCallback(callback){
-  onSecondCallback = callback;
-}
-
-function setOnStartCallback(callback){
-  onStopCallback = callback;
-}
-
-function setOnStopCallback(callback){
-  onStopCallback = callback;
-}
-
-function setRenderCallback(callback){
-  render = callback;
-}
-
-function setInterval(time){
-  interval = time;
-  render(interval)
-}
-
-function start(){
-  startTime = Date.now();
-  lastStatus = startTime;
-  timeout = nextFrame(loop, 0);
-  return status;
-}
-
-function pause(){
-  interval = status;
-  cancelFrame(timeout);
-  return status;
-}
-
-function resume(){
-  startTime = Date.now();
-  timeout = nextFrame(loop, 0);
-  return status;
-}
-
-function stop(){
-  status = 0;
-  cancelFrame(timeout);
-  renderer.render(status);
-  onStopCallback();
-  return status;
-}
-
-function loop(){
-  lastStatus = status;
-  const current = new Date().getTime();
-  const passed = current - startTime;
-  status = Math.max(interval - passed, 0);
-  renderer.render(status);
-  if(Math.floor(status / 1000) !== Math.floor(lastStatus / 1000)){
-    onSecondCallback(Math.floor(lastStatus / 1000));
+  function setRenderer(_renderer){
+    renderer = _renderer;
   }
-  if(status <= 0){
-    console.log('finished!');
-    stop();
-  } else {
-    timeout = nextFrame(loop);
-  }
-}
 
-exports.timer = {
-  start,
-  pause,
-  resume,
-  stop,
-  setRenderer,
-  setRenderCallback,
-  setOnStopCallback,
-  setOnSecondCallback,
-  setInterval
-};
+  function setOnSecondCallback(callback){
+    onSecondCallback = callback;
+  }
+
+  function setOnStartCallback(callback){
+    onStopCallback = callback;
+  }
+
+  function setOnStopCallback(callback){
+    onStopCallback = callback;
+  }
+
+  function setRenderCallback(callback){
+    render = callback;
+  }
+
+  function reset(time){
+    if(time){
+      limit = time;
+    }
+    status = interval = limit;
+    renderer.render(limit);
+  }
+
+  function start(){
+    if(status <= 0){
+      return;
+    }
+    startTime = Date.now();
+    lastStatus = startTime;
+    timeout = nextFrame(loop, 0);
+    return status;
+  }
+
+  function pause(){
+    interval = status;
+    cancelFrame(timeout);
+    return status;
+  }
+
+  function stop(){
+    status = 0;
+    cancelFrame(timeout);
+    renderer.render(status);
+    onStopCallback();
+    return status;
+  }
+
+  function loop(){
+    lastStatus = status;
+    const current = new Date().getTime();
+    const passed = current - startTime;
+    status = Math.max(interval - passed, 0);
+    renderer.render(status);
+    if(Math.floor(status / 1000) !== Math.floor(lastStatus / 1000)){
+      onSecondCallback(Math.floor(lastStatus / 1000));
+    }
+    if(status <= 0){
+      stop();
+    } else {
+      timeout = nextFrame(loop);
+    }
+  }
+
+  module.exports = {
+    start,
+    pause,
+    stop,
+    reset,
+    setRenderer,
+    setRenderCallback,
+    setOnStopCallback,
+    setOnSecondCallback
+  };
+})();
--- src/timerController.js
+++ src/timerController.js
@@ -1,76 +1,88 @@
-'use strict';
-const $targetTime = $('#time');
-const $targetMsec = $('#time-millis');
-const $content = $('.page-content');
+(function($){
+  /* global h5, window */
+  'use strict';
+  // TODO: jQueryオブジェクトのキャッシュを集約する(コントローラ内が望ましい？）
+  const $targetTime = $('#time');
+  const $targetMsec = $('#time-millis');
+  const $content = $('.page-content');
 
-const renderer = {
-  formatter : require('./formatter.js'),
-  $targets: {
-    $time: $targetTime,
-    $msec: $targetMsec,
-  },
-  render: function(time){
-    this.$targets.$time.text(this.formatter.formatTimeString(time));
-    this.$targets.$msec.text(this.formatter.formatMsecString(time));
-  }
-};
+  const renderer = {
+    formatter : require('./formatter.js'),
+    $targets: {
+      $time: $targetTime,
+      $msec: $targetMsec,
+    },
+    render: function(time){
+      this.$targets.$time.text(this.formatter.formatTimeString(time));
+      this.$targets.$msec.text(this.formatter.formatMsecString(time));
+    }
+  };
 
-const formatter = require('./formatter.js');
-const onStartCallback = (function($content){
-  $content.removeClass('warn danger');
-})($content)
-const onSecondCallback = (function($con){
-  return function(time){
-    if(time <= 15){
-      $con.removeClass('warn danger').addClass('danger');
-    } else if(time <= 30){
-      $con.removeClass('warn danger').addClass('warn');
+  const onStartCallback = (function($content){
+    $content.removeClass('warn danger');
+  })($content)
+  const onSecondCallback = (function($con){
+    return function(time){
+      if(time <= 15){
+        $con.removeClass('warn danger').addClass('danger');
+      } else if(time <= 30){
+        $con.removeClass('warn danger').addClass('warn');
+      }
+    }
+  })($content)
+  const onStopCallback = (function($time, $msec){
+    return function(){
+      $('#resetBtn').show();
+      $('#pauseBtn').hide();
+      $('#startBtn').hide();
+      $msec.empty();
+      $time.text('DONE!');
+    }
+  })($targetTime, $targetMsec, $content)
+
+  const timerController = {
+    __name: 'ltimer.controller.timerController',
+    _timer: null,
+    __ready: function(context){
+      this._timer = require('./timer.js');
+      this._timer.setRenderer(renderer);
+      this._timer.setOnSecondCallback(onSecondCallback);
+      this._timer.setOnStopCallback(onStopCallback);
+      this._timer.reset(31000);
+    },
+
+    _render: function(time, $target){
+      $target.text(this._formatter.formatTimeString(time));
+    },
+
+    '#startBtn _click': function(context, $el) {
+      this.$find('#startBtn').hide();
+      this.$find('#pauseBtn').show();
+      this.$find('#stopBtn').show();
+      this._timer.start();
+    },
+
+    '#pauseBtn _click': function(context, $el) {
+      this.$find('#pauseBtn').hide();
+      this.$find('#stopBtn').show();
+      this.$find('#startBtn').show();
+      this._timer.pause();
+    },
+
+    '#stopBtn _click': function(context, $el) {
+      this._timer.stop();
+    },
+
+    '#resetBtn _click': function(context, $el) {
+      this.$find('#stopBtn').hide();
+      this.$find('#resetBtn').hide();
+      this.$find('#pauseBtn').hide();
+      this.$find('#startBtn').show();
+      $content.removeClass('warn danger');
+      this._timer.reset();
     }
   }
-})($content)
-const onStopCallback = (function($time, $msec){
-  return function(){
-    $msec.empty();
-    $time.text('DONE!');
-  }
-})($targetTime, $targetMsec, $content)
 
-const timerController = {
-  __name: 'ltimer.controller.timerController',
-  _timer: null,
-  __ready: function(context){
-    this._timer = require('./timer.js');
-    this._timer.timer.setRenderer(renderer);
-    this._timer.timer.setOnSecondCallback(onSecondCallback);
-    this._timer.timer.setOnStopCallback(onStopCallback);
-    this._timer.timer.setInterval(300000);
-  },
-
-  _render: function(time, $target){
-    $target.text(this._formatter.formatTimeString(time));
-  },
-
-  '#startBtn click': function(context, $el) {
-    this.$find('#startBtn').hide();
-    this.$find('#pauseBtn').show();
-    this.$find('#stopBtn').show();
-    this._timer.timer.start();
-  },
-
-  '#pauseBtn click': function(context, $el) {
-    this.$find('#pauseBtn').hide();
-    this.$find('#stopBtn').show();
-    this.$find('#startBtn').show();
-    this._timer.timer.pause();
-  },
-
-  '#stopBtn click': function(context, $el) {
-    this.$find('#stopBtn').hide();
-    this.$find('#pauseBtn').hide();
-    this.$find('#startBtn').show();
-    this._timer.timer.stop();
-  }
-}
-
-// ページの読み込みが終わったらコントローラをバインドする
-$(function(){h5.core.controller('#timer', timerController);})
+  // ページの読み込みが終わったらコントローラをバインドする
+  $(function(){h5.core.controller('#timer', timerController);})
+})(jQuery);
